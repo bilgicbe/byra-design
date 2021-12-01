68a842e8cea7c60ecbe95eda927a9876bb55ca47
Bekir <95bekirable@gmail.com>
2021-12-01 05:05:09 +0300

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const burger = document.querySelector(".burger") 
+const burger = document.querySelector(".burger")
 const menuLink = document.querySelectorAll(".menu-link")
 const hiddenMenu = document.querySelector(".hidden-menu")
-const slidingLoader = document.querySelector(".sliding-loader")
+const heroSlider = document.querySelector(".sliding-hero-container")
+const slidingBackground = document.querySelectorAll(".sliding>.select")
 
 function handleBurgerClick() {
     this.classList.toggle("active")
@@ -27,3 +28,37 @@ menuLink.forEach((element) => {
     element.addEventListener("mouseover", handleMenuHover)
     element.addEventListener("mouseout", handleMenuOut)
 })
+
+function handleHeroSlide() {
+    console.log(heroSlider.style.animationName)
+    if (heroSlider.style.animationName == 'slide') {
+        heroSlider.style.animationName = 'slide2'
+    } else {
+        heroSlider.style.animationName = 'slide'
+    }
+}
+
+function handleElementDrag() {
+    heroSlider.appendChild(heroSlider.firstElementChild)
+    
+    handleBackgroundTransition()
+    document.querySelector('.select').style.transition = 'none'
+    document.querySelector('.trans-toggle').classList.toggle('trans-toggle')
+    setTimeout(()=>{
+        document.querySelector('.select').style.transition = 'background-size 4s linear'
+    },10)
+}
+
+
+function handleBackgroundTransition() {
+    document.querySelector('.trans-toggle').parentElement.nextElementSibling.firstElementChild.classList.toggle('trans-toggle')
+}
+
+heroSlider.addEventListener("animationend", handleElementDrag)
+slidingBackground.forEach((element)=> {
+    element.addEventListener("transitionend", handleHeroSlide)
+})
+
+setTimeout(()=>{
+    document.querySelector('.hero-slider-background-1').classList.toggle('trans-toggle')
+},500)
