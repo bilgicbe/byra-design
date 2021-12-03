93fccafcbf3712a6c9044ff44dc7edf8e2f2bbfc
Bekir <95bekirable@gmail.com>
2021-12-03 07:12:08 +0300

    6th commit

--- script.js
+++ script.js
@@ -5,13 +5,36 @@ const heroSlider = document.querySelector(".sliding-hero-container")
 const slidingBackground = document.querySelectorAll(".sliding>.select")
 const scrollButton = document.querySelector(".scroll-button")
 
+document.querySelector(".menu-footer").style.animationName = "menu-footer-animation"
 function handleBurgerClick() {
-    this.classList.toggle("active")
-    this.classList.toggle("inactive")
-    document.querySelector(".hidden-menu").classList.toggle("active")
+    burger.classList.toggle("active")
+    let menuFooterAnimation = document.querySelector(".menu-footer").style.animationName
+    if (menuFooterAnimation == "menu-footer-animation"){
+        document.querySelector(".menu-footer").style.animationName = ""
+        document.querySelectorAll(".menu-footer p, .menu-footer h2").forEach((element)=> element.style.animationName="menu-footer-entry")
+        document.querySelector(".menu-link.menu-link-1").style.animationName = "menulink1"
+        document.querySelector(".menu-link.menu-link-2").style.animationName = "menulink2"
+        document.querySelector(".menu-link.menu-link-3").style.animationName = "menulink3"
+        document.querySelector(".menu-link.menu-link-4").style.animationName = "menulink4"
+        document.querySelector(".menu-link.menu-link-5").style.animationName = "menulink5"
+    } else {
+        document.querySelector(".menu-footer").style.animationName = "menu-footer-animation"
+        document.querySelectorAll(".menu-footer p, .menu-footer h2").forEach((element)=> element.style.animationName="")
+        document.querySelector(".menu-link.menu-link-1").style.animationName = ""
+        document.querySelector(".menu-link.menu-link-2").style.animationName = ""
+        document.querySelector(".menu-link.menu-link-3").style.animationName = ""
+        document.querySelector(".menu-link.menu-link-4").style.animationName = ""
+        document.querySelector(".menu-link.menu-link-5").style.animationName = ""
+    }
+    burger.classList.toggle("inactive")
+    document.querySelector(".menu-body").classList.toggle("active")
+    setTimeout(()=>{
+        document.querySelector(".hidden-menu").classList.toggle("active")
+    },650)
 }
 
 burger.addEventListener("click", handleBurgerClick)
+document.querySelectorAll(".menu-link a").forEach((element)=>element.addEventListener("click",handleBurgerClick))
 
 function handleMenuHover() {
     this.querySelector(".menu-number").style.color = "goldenrod"
@@ -31,11 +54,13 @@ menuLink.forEach((element) => {
 })
 
 function handleHeroSlide() {
-    if (heroSlider.style.animationName == 'slide') {
-        heroSlider.style.animationName = 'slide2'
-    } else {
-        heroSlider.style.animationName = 'slide'
-    }
+    setTimeout(()=>{
+        if (heroSlider.style.animationName == 'slide') {
+            heroSlider.style.animationName = 'slide2'
+        } else {
+            heroSlider.style.animationName = 'slide'
+        }
+    },1000)
 }
 
 function handleElementDrag() {
@@ -45,13 +70,13 @@ function handleElementDrag() {
     document.querySelector('.select').style.transition = 'none'
     document.querySelector('.trans-toggle').classList.toggle('trans-toggle')
     setTimeout(() => {
-        document.querySelector('.select').style.transition = 'background-size 4s linear'
+        document.querySelector('.select').style.transition = 'transform 4s linear'
     }, 10)
 }
 
 
 function handleBackgroundTransition() {
-    document.querySelector('.trans-toggle').parentElement.nextElementSibling.firstElementChild.classList.toggle('trans-toggle')
+    document.querySelector('.trans-toggle').nextElementSibling.classList.toggle('trans-toggle')
 }
 
 heroSlider.addEventListener("animationend", handleElementDrag)
@@ -60,7 +85,7 @@ slidingBackground.forEach((element) => {
 })
 
 setTimeout(() => {
-    document.querySelector('.hero-slider-background-1').classList.toggle('trans-toggle')
+    document.querySelector('.hero-slider-1').classList.toggle('trans-toggle')
 }, 1200)
 
 const loadingScreen = document.querySelector(".loading-screen")
@@ -72,7 +97,7 @@ setTimeout(() => {
 window.addEventListener("scroll", () => {
     let toTop = document.querySelector(".to-top")
     let toAbout = document.querySelector(".to-about")
-    if (window.pageYOffset > document.querySelector(".about-container").offsetTop) {
+    if (window.pageYOffset > document.querySelector(".about-container").offsetTop - 5) {
         toTop.style.display = "flex"
         toAbout.style.display = "none"
     } else {
@@ -81,7 +106,7 @@ window.addEventListener("scroll", () => {
     }
 })
 
-var aboutSection = document.getElementById("about")
+var aboutSection = document.getElementById("About")
 var topSection = document.getElementById("body")
 document.querySelector(".to-top").addEventListener("click", () => topSection.scrollIntoView())
 document.querySelector(".to-about").addEventListener("click", () => aboutSection.scrollIntoView())
@@ -102,12 +127,10 @@ aboutUsImage.forEach((element) => {
 })
 
 const portfolioLinks = document.querySelectorAll(".portfolio-link")
-console.log(document.querySelector(".screen-1").style.animationName)
 portfolioLinks.forEach((element) => {
     element.addEventListener("mouseover", () => element.querySelector(".line-absolute").style.width = "100%")
     element.addEventListener("mouseout", () => element.querySelector(".line-absolute").style.width = "0%")
     element.addEventListener("click", () => {
-        console.log(document.querySelector(".screen-1").style.animationName)
         document.querySelector(".screen-1").style.animationName = "reverse1"
         document.querySelector(".screen-2").style.animationName = "reverse2"
         document.querySelector(".screen-3").style.animationName = "reverse3"
@@ -119,16 +142,96 @@ portfolioLinks.forEach((element) => {
     })
 })
 
-portfolioLinks[0].addEventListener("mouseover", () => document.querySelector(".t01").style.opacity = "1")
-portfolioLinks[0].addEventListener("mouseout", () => document.querySelector(".t01").style.opacity = "0")
-portfolioLinks[1].addEventListener("mouseover", () => document.querySelector(".t02").style.opacity = "1")
-portfolioLinks[1].addEventListener("mouseout", () => document.querySelector(".t02").style.opacity = "0")
-portfolioLinks[2].addEventListener("mouseover", () => document.querySelector(".t03").style.opacity = "1")
-portfolioLinks[2].addEventListener("mouseout", () => document.querySelector(".t03").style.opacity = "0")
-portfolioLinks[3].addEventListener("mouseover", () => document.querySelector(".t04").style.opacity = "1")
-portfolioLinks[3].addEventListener("mouseout", () => document.querySelector(".t04").style.opacity = "0")
-portfolioLinks[4].addEventListener("mouseover", () => document.querySelector(".t05").style.opacity = "1")
-portfolioLinks[4].addEventListener("mouseout", () => document.querySelector(".t05").style.opacity = "0")
+portfolioLinks[0].addEventListener("mouseover", () => {
+    document.querySelector(".t01").style.opacity = "1"
+    document.querySelector(".first-portfolio-change.change1").style.opacity = "1"
+    document.querySelector(".first-portfolio-change.change1").style.transform = "rotate(0deg) scale(1)"
+    document.querySelector(".second-portfolio-change.change1").style.opacity = "1"
+    document.querySelector(".second-portfolio-change.change1").style.transform = "rotate(0deg) scale(1)"
+    document.querySelector(".third-portfolio-change.change1").style.opacity = "1"
+    document.querySelector(".third-portfolio-change.change1").style.transform = "rotate(0deg) scale(1)"
+})
+portfolioLinks[0].addEventListener("mouseout", () => {
+    document.querySelector(".t01").style.opacity = "0"
+    document.querySelector(".first-portfolio-change.change1").style.opacity = "0"
+    document.querySelector(".first-portfolio-change.change1").style.transform = "rotate(-20deg) scale(1.7)"
+    document.querySelector(".second-portfolio-change.change1").style.opacity = "0"
+    document.querySelector(".second-portfolio-change.change1").style.transform = "rotate(-20deg) scale(1.7)"
+    document.querySelector(".third-portfolio-change.change1").style.opacity = "0"
+    document.querySelector(".third-portfolio-change.change1").style.transform = "rotate(-20deg) scale(1.7)"
+})
+portfolioLinks[1].addEventListener("mouseover", () => {
+    document.querySelector(".t02").style.opacity = "1"
+    document.querySelector(".first-portfolio-change.change2").style.opacity = "1"
+    document.querySelector(".first-portfolio-change.change2").style.transform = "rotate(0deg) scale(1)"
+    document.querySelector(".second-portfolio-change.change2").style.opacity = "1"
+    document.querySelector(".second-portfolio-change.change2").style.transform = "rotate(0deg) scale(1)"
+    document.querySelector(".third-portfolio-change.change2").style.opacity = "1"
+    document.querySelector(".third-portfolio-change.change2").style.transform = "rotate(0deg) scale(1)"
+})
+portfolioLinks[1].addEventListener("mouseout", () => {
+    document.querySelector(".t02").style.opacity = "0"
+    document.querySelector(".first-portfolio-change.change2").style.opacity = "0"
+    document.querySelector(".first-portfolio-change.change2").style.transform = "rotate(-20deg) scale(1.7)"
+    document.querySelector(".second-portfolio-change.change2").style.opacity = "0"
+    document.querySelector(".second-portfolio-change.change2").style.transform = "rotate(-20deg) scale(1.7)"
+    document.querySelector(".third-portfolio-change.change2").style.opacity = "0"
+    document.querySelector(".third-portfolio-change.change2").style.transform = "rotate(-20deg) scale(1.7)"
+})
+portfolioLinks[2].addEventListener("mouseover", () => {
+    document.querySelector(".t03").style.opacity = "1"
+    document.querySelector(".first-portfolio-change.change3").style.opacity = "1"
+    document.querySelector(".first-portfolio-change.change3").style.transform = "rotate(0deg) scale(1)"
+    document.querySelector(".second-portfolio-change.change3").style.opacity = "1"
+    document.querySelector(".second-portfolio-change.change3").style.transform = "rotate(0deg) scale(1)"
+    document.querySelector(".third-portfolio-change.change3").style.opacity = "1"
+    document.querySelector(".third-portfolio-change.change3").style.transform = "rotate(0deg) scale(1)"
+})
+portfolioLinks[2].addEventListener("mouseout", () => {
+    document.querySelector(".t03").style.opacity = "0"
+    document.querySelector(".first-portfolio-change.change3").style.opacity = "0"
+    document.querySelector(".first-portfolio-change.change3").style.transform = "rotate(-20deg) scale(1.7)"
+    document.querySelector(".second-portfolio-change.change3").style.opacity = "0"
+    document.querySelector(".second-portfolio-change.change3").style.transform = "rotate(-20deg) scale(1.7)"
+    document.querySelector(".third-portfolio-change.change3").style.opacity = "0"
+    document.querySelector(".third-portfolio-change.change3").style.transform = "rotate(-20deg) scale(1.7)"
+})
+portfolioLinks[3].addEventListener("mouseover", () => {
+    document.querySelector(".t04").style.opacity = "1"
+    document.querySelector(".first-portfolio-change.change4").style.opacity = "1"
+    document.querySelector(".first-portfolio-change.change4").style.transform = "rotate(0deg) scale(1)"
+    document.querySelector(".second-portfolio-change.change4").style.opacity = "1"
+    document.querySelector(".second-portfolio-change.change4").style.transform = "rotate(0deg) scale(1)"
+    document.querySelector(".third-portfolio-change.change4").style.opacity = "1"
+    document.querySelector(".third-portfolio-change.change4").style.transform = "rotate(0deg) scale(1)"
+})
+portfolioLinks[3].addEventListener("mouseout", () => {
+    document.querySelector(".t04").style.opacity = "0"
+    document.querySelector(".first-portfolio-change.change4").style.opacity = "0"
+    document.querySelector(".first-portfolio-change.change4").style.transform = "rotate(-20deg) scale(1.7)"
+    document.querySelector(".second-portfolio-change.change4").style.opacity = "0"
+    document.querySelector(".second-portfolio-change.change4").style.transform = "rotate(-20deg) scale(1.7)"
+    document.querySelector(".third-portfolio-change.change4").style.opacity = "0"
+    document.querySelector(".third-portfolio-change.change4").style.transform = "rotate(-20deg) scale(1.7)"
+})
+portfolioLinks[4].addEventListener("mouseover", () => {
+    document.querySelector(".t05").style.opacity = "1"
+    document.querySelector(".first-portfolio-change.change5").style.opacity = "1"
+    document.querySelector(".first-portfolio-change.change5").style.transform = "rotate(0deg) scale(1)"
+    document.querySelector(".second-portfolio-change.change5").style.opacity = "1"
+    document.querySelector(".second-portfolio-change.change5").style.transform = "rotate(0deg) scale(1)"
+    document.querySelector(".third-portfolio-change.change5").style.opacity = "1"
+    document.querySelector(".third-portfolio-change.change5").style.transform = "rotate(0deg) scale(1)"
+})
+portfolioLinks[4].addEventListener("mouseout", () => {
+    document.querySelector(".t05").style.opacity = "0"
+    document.querySelector(".first-portfolio-change.change5").style.opacity = "0"
+    document.querySelector(".first-portfolio-change.change5").style.transform = "rotate(-20deg) scale(1.7)"
+    document.querySelector(".second-portfolio-change.change5").style.opacity = "0"
+    document.querySelector(".second-portfolio-change.change5").style.transform = "rotate(-20deg) scale(1.7)"
+    document.querySelector(".third-portfolio-change.change5").style.opacity = "0"
+    document.querySelector(".third-portfolio-change.change5").style.transform = "rotate(-20deg) scale(1.7)"
+})
 
 const zoomedPortfolio = document.querySelector(".zoomed-portfolio")
 portfolioLinks[0].addEventListener("click", () => {
@@ -156,6 +259,75 @@ document.querySelector(".cross").addEventListener("click", () => {
         zoomedPortfolio.style.opacity = '0'
         zoomedPortfolio.style.pointerEvents = "none"
     }, 1000)
-    let portfolioSection = document.getElementById("portfolio")
+    let portfolioSection = document.getElementById("Portfolio")
     portfolioSection.scrollIntoView()
 })
+
+const teamCarousel = document.querySelector(".our-team-slide-container") 
+function handleButtonSelect(rate){
+    document.querySelectorAll(".round-buttons div").forEach((element)=>element.classList.remove("selected-button"))
+    teamCarousel.style.transform = `translate(${rate}px)`
+    document.querySelectorAll(".our-team-slides").forEach((element)=>{element.classList.remove("focused")})
+}
+
+document.querySelector(".button-1").addEventListener("click", ()=>{
+    handleButtonSelect(0)
+    document.querySelector(".button-1").classList.add("selected-button")
+    document.querySelector(".our-team-slide-1").classList.add("focused")
+})
+document.querySelector(".button-2").addEventListener("click", ()=>{
+    handleButtonSelect(-710)
+    document.querySelector(".button-2").classList.add("selected-button")
+    document.querySelector(".our-team-slide-2").classList.add("focused")
+})
+document.querySelector(".button-3").addEventListener("click", ()=>{
+    handleButtonSelect(-1420)
+    document.querySelector(".button-3").classList.add("selected-button")
+    document.querySelector(".our-team-slide-3").classList.add("focused")
+})
+document.querySelector(".button-4").addEventListener("click", ()=>{
+    handleButtonSelect(-2130)
+    document.querySelector(".button-4").classList.add("selected-button")
+    document.querySelector(".our-team-slide-4").classList.add("focused")
+})
+
+document.querySelector(".next-container").addEventListener("click", () => {
+    if (document.querySelector(".selected-button").nextElementSibling==null) {
+        // teamCarousel.appendChild(teamCarousel.firstElementChild)
+        // teamCarousel.appendChild(teamCarousel.firstElementChild)
+        // handleButtonSelect(-1420)
+        // document.querySelector(".our-team-slide-1").classList.add("focused")
+        // document.querySelector(".button-1").classList.add("selected-button")
+    } else {
+    document.querySelector(".selected-button").nextElementSibling.click()
+    }
+})
+document.querySelector(".prev-container").addEventListener("click", () => {
+    if (document.querySelector(".selected-button").previousElementSibling==null) {
+        // teamCarousel.insertBefore(teamCarousel.lastElementChild,teamCarousel.firstElementChild)
+        // teamCarousel.insertBefore(teamCarousel.lastElementChild,teamCarousel.firstElementChild)
+        // handleButtonSelect(-710)
+        // document.querySelector(".our-team-slide-4").classList.add("focused")
+        // document.querySelector(".button-4").classList.add("selected-button")
+    } else {
+    document.querySelector(".selected-button").previousElementSibling.click()
+    }
+})
+
+
+
+const awardsParagraphy = document.querySelector(".awwards-paragraph")
+
+function handleAwardsOver() {
+    this.querySelector(".awards-line").style.width = "100px"
+    this.querySelector(".awwards-title").style.color = "goldenrod"
+    awardsParagraphy.style.opacity = 1
+}
+function handleAwardsOut() {
+    this.querySelector(".awards-line").style.width = "0px"
+    this.querySelector(".awwards-title").style.color = "white"
+    awardsParagraphy.style.opacity = 0
+}
+
+document.querySelectorAll(".awards-link").forEach((element) => element.addEventListener("mouseover", handleAwardsOver))
+document.querySelectorAll(".awards-link").forEach((element) => element.addEventListener("mouseout", handleAwardsOut))
